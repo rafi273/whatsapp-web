a28e4cdd0350f6dabae97ed22dbd9c6d29ef3b72
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2022-12-04 00:07:23 +0700

    add socket

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import errorHandler from "errorhandler";
-import http from "http";
-import socketIo from "socket.io";
 import app from "./app";
-import client from "./socket";
+import "./socket";
 import "dotenv/config";
 
 
@@ -17,15 +15,6 @@ if (process.env.NODE_ENV === "development") {
 /**
  * Start Express server.
  */
-const serv = http.createServer(app);
-const io = socketIo(serv);
-
-client.initialize();
-io.on("connection", function(socket) {
-    console.log("Connecting...");
-    socket.emit("message", "Connecting..."); 
-});
-
 const server = app.listen(app.get("port"), () => {
     console.log(
         "  App is running at http://localhost:%d in %s mode",
--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,7 @@
 import { Client, LocalAuth } from "whatsapp-web.js";
-import qrcode from "qrcode-terminal";
+import qrcodeTerminal from "qrcode-terminal";
+import qrcode from "qrcode";
+import ws from "ws";
 import "dotenv/config";
 
 // Controllers (route handlers)
@@ -18,32 +20,58 @@ const client = new Client({
     },
     authStrategy: new LocalAuth()
 });
+const io = new ws.WebSocketServer({
+    port: 3001
+});
+
+client.initialize();
+
+io.on("connection", function(socket) {
+    socket.send(template(notification, "Connecting...")); 
+
+    client.on("qr", (qr) => {
+        qrcode.toDataURL(qr, (err, url) => {
+            socket.emit("qr", url);
+            socket.emit("message", "QR Code received, scan please!");
+        });
+    });
+
+    client.on("ready", () => {
+        socket.emit("ready", "Whatsapp is ready!");
+        socket.emit("message", "Whatsapp is ready!");
+    });
+    
+    client.on("authenticated", () => {
+        socket.emit("authenticated", "Whatsapp is authenticated!");
+        socket.emit("message", "Whatsapp is authenticated!");
+    });
+
+    client.on("auth_failure", () => {
+        socket.emit("message", "Auth failure, restarting...");
+    });
+
+    client.on("disconnected", () => {
+        socket.emit("message", "Whatsapp is disconnected!");
+        client.destroy();
+        client.initialize();
+    });
+});
+
+const notification = "notification";
+
+function template(event: string, message: string) {
+    return JSON.stringify({
+        event: event,
+        payload: message
+    });
+}
 
 client.on("loading_screen", (percent, message) => {
     console.log("LOADING SCREEN", percent, message);
 });
 
-client.on("qr", (qr) => {
-    // NOTE: This event will not be fired if a session is specified.
-    console.log("QR RECEIVED", qr);
-    qrcode.generate(qr, {small: true});
-});
-
-client.on("auth_failure", message => {
-    // Fired if session restore was unsuccessful
-    console.error("AUTHENTICATION FAILURE", message);
-});
-
-client.on("ready", () => {
-    console.log("Client is ready!");
-});
-
 client.on("message", message => {
     bot(client, message);
-});
-
-client.on("disconnected", (reason) => {
-    console.log("Client was logged out", reason);
 });
 
 export default client;
