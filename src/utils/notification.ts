cfbaad9e6e80f1cefe0dc1a622433ef0baf47aa0
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2022-12-05 13:06:08 +0700

    fix

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,8 +1,8 @@
 import { Client } from "whatsapp-web.js";
 import { sendMessage } from "../models/message";
 
-export function textHandling(client: Client, chatId:string) {
-    sendMessage(client, chatId, "Mohon hanya pilih jawaban yang tersedia");
+export async function textHandling(client: Client, chatId:string) {
+    await sendMessage(client, chatId, "Mohon hanya pilih jawaban yang tersedia");
 }
 
 export function endMessage(message?: string): string[] {
