c540abb40494e8db95dd06f34d2e13ad9855ce8c
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2022-12-06 17:55:35 +0700

    fix

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -4,8 +4,8 @@ import { Request, Response } from "express";
  * Home page.
  * @route GET /
  */
-export const index = (req: Request, res: Response) => {
-    res.render("home", {
+export async function index(req: Request, res: Response) {
+    return res.render("home", {
         socketHost: process.env.SOCKET_HOST ?? "ws://localhost:3001"
     });
-};
+}
