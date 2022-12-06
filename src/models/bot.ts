d0e62d5d1e63e10fdc52176904a3ecb3f4ead9c6
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2022-12-06 16:37:53 +0700

    fix

--- src/models/bot.ts
+++ src/models/bot.ts
@@ -329,22 +329,22 @@ export async function bot(client: Client, message: WAWebJS.Message): Promise<voi
 
         switch (data.question_choice) {
             case "province":
-                data.question_choice = temp.provinceData ? temp.provinceData : temp.provinceData.map((value: any) => value.province);
+                data.question_choice = temp.provinceData.length ? temp.provinceData : temp.provinceData.map((value: any) => value.province);
 
                 break;
 
             case "city":
-                data.question_choice = temp.cityData ? temp.cityData : temp.cityData.map((value: any) => value.city);
+                data.question_choice = temp.cityData.length ? temp.cityData : temp.cityData.map((value: any) => value.city);
 
                 break;
 
             case "districts":
-                data.question_choice = temp.districtsData ? temp.districtsData : temp.districtsData.map((value: any) => value.districts);
+                data.question_choice = temp.districtsData.length ? temp.districtsData : temp.districtsData.map((value: any) => value.districts);
 
                 break;
 
             case "urban village":
-                data.question_choice = temp.urbanVillageData ? temp.urbanVillageData : temp.urbanVillageData.map((value: any) => value.urban_village);
+                data.question_choice = temp.urbanVillageData.length ? temp.urbanVillageData : temp.urbanVillageData.map((value: any) => value.urban_village);
 
                 break;
         }
